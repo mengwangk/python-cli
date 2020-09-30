d33778fc32ce1f507abc2a64d0fde980b1b385c6
Koh Meng Wang <mengwangk@gmail.com>
2020-09-30 15:26:55 +0800

    Update cli

--- cli.py
+++ cli.py
@@ -13,21 +13,21 @@ def cli():
 
 
 @click.command()
-@click.argument('src_db')
-@click.argument('dest_db')
+@click.argument("src_db")
+@click.argument("dest_db")
 def command1(src_db, dest_db):
     """Command 1."""
-    click.echo('command 1 ')
+    click.echo("command 1 ")
     click.echo(src_db)
     click.echo(dest_db)
 
 
 @click.command()
-@click.argument('src_db')
-@click.argument('dest_db')
+@click.argument("src_db")
+@click.argument("dest_db")
 def command2(src_db, dest_db):
     """Command 2."""
-    click.echo('command 2')
+    click.echo("command 2")
     click.echo(src_db)
     click.echo(dest_db)
     print(settings.TEST_CONFIG)
@@ -36,5 +36,5 @@ def command2(src_db, dest_db):
 cli.add_command(command1)
 cli.add_command(command2)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
